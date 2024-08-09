91bf27c414eca4e9cb5248c68c8a4451222fc601
bri-anadi <uix.brianadi@gmail.com>
2024-08-09 15:31:53 +0700

    add note HelloWorld.sol

--- 01-HelloWorld/HelloWorld.sol
+++ 01-HelloWorld/HelloWorld.sol
@@ -1,6 +1,13 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+// Lisensi yang digunakan adalah MIT dengan compiler solidity 0.8;
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+// membuat sebuah contract dengan nama HelloWorld
 contract HelloWorld {
+    
+    // 'string' merupakan tipe data yang mendefinisikan kalimat atau huruf
+    // 'public' berarti tipe data dapat diakses secara global yang artinya siapa saja dapat membaca value dari variable
+    // 'Hello World' merupakan nilai dari variable myString
     string public myString = "Hello World";
 }
