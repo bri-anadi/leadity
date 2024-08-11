e405ada7fddf2f5301fe0499d0e57e4168999bf4
bri-anadi <uix.brianadi@gmail.com>
2024-08-11 17:33:07 +0700

    add state variable

---
+++ 04-StateVariables/StateVariable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// state variable secara sederhana adalah menyimpan data kedalam blockchain
+// dapat bertipe lokal maupun global variable
+// state variable disimpan permanen
+
+contract StateVariable {
+    // contoh state variable
+    uint public myInt = 123;
+
+    function localVariable() pure external {
+        // local variable berarti variable hanya dapat dipanggil dalam fungsi itu sendiri
+        uint integer = 12345;
+        integer;
+    }
+}
